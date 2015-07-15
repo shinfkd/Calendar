63fe88a828c6e8561f93887acb581b82a7296894
Shintaro Fukuda <shin.fkd@gmail.com>
2015-07-15 20:02:38 +0900

    Initial Commit

---
+++ Calendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Shintaro Fukuda on 2015/07/15.
+//  Copyright (c) 2015年 Livesense Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
