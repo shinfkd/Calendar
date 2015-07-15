f29a1e9e7a7932d53c24287873311587b470ba07
Shintaro Fukuda <shin.fkd@gmail.com>
2015-07-15 21:15:17 +0900

    first commit

---
+++ Calendar/DayView.swift
@@ -0,0 +1,21 @@
+//
+//  DayView.swift
+//  Calendar
+//
+//  Created by Shintaro Fukuda on 2015/07/15.
+//  Copyright (c) 2015年 Livesense Inc. All rights reserved.
+//
+
+import UIKit
+
+class DayView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
